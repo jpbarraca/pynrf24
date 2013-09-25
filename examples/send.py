707ed24076f330be908d692a8d8bea9a0c0357af
João Paulo Barraca <jpbarraca@ua.pt>
2013-09-26 00:12:05 +0100

    Added send example

---
+++ examples/send.py
@@ -0,0 +1,32 @@
+#
+# Example program to send packets to the radio
+#
+# João Paulo Barraca <jpbarraca@gmail.com>
+#
+
+from nrf24 import NRF24
+import time
+
+pipes = [[0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]]
+
+radio = NRF24.NRF24()
+radio.begin(1, 0, "P9_15", "P9_16") #Set CE and IRQ pins
+radio.setRetries(15,15)
+radio.setPayloadSize(8)
+radio.setChannel(0x60)
+
+radio.setDataRate(NRF24.NRF24.BR_250KBPS)
+radio.setPALevel(NRF24.NRF24.PA_MAX)
+
+radio.openWritingPipe(pipes[1])
+radio.openReadingPipe(1, pipes[0])
+
+radio.startListening()
+radio.stopListening()
+
+radio.printDetails()
+
+buf = ['P', 'I', 'N', 'G']
+while True:
+    radio.write(buf)
+    time.sleep(1)
