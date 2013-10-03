8ea3f7e1e2d1d08487d3e73774e9955db9a96506
João Paulo Barraca <jpbarraca@ua.pt>
2013-10-03 22:56:44 +0100

    Added license

--- nrf24.py
+++ nrf24.py
@@ -1,9 +1,18 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
 # Python port of Maniacbug NRF24L01 library
 # Author: Joao Paulo Barraca <jpbarraca@gmail.com>
 #
 
-import Adafruit_BBIO.GPIO as GPIO 
+import Adafruit_BBIO.GPIO as GPIO
 import spidev
 import time
 import sys
