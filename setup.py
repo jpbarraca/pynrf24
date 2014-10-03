9883d8501b58c3ce78c567570293952681e710b3
Stefan Lau <github@stefanlau.com>
2014-10-03 14:30:45 +0200

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='nrf24',
+      version='0.0.1',
+      description='Python port of the RF24 library for NRF24L01+ radios',
+      py_modules=['nrf24'],
+     )
